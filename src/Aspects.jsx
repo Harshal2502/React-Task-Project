81adf8ebf3d6dd416db8ad20bc8641b3469d1e07
unknown <devmurariharshal2002@gmail.com>
2022-08-17 13:28:48 +0530

    Aspect_Section_Complte

--- src/Aspects.jsx
+++ src/Aspects.jsx
@@ -1,6 +1,15 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function Aspects() {
+
+    const [width, setWidth] = useState(window.innerWidth);
+    const updateWidth = () => {
+        setWidth(window.innerWidth);
+    }
+    useEffect(() => {
+        window.addEventListener("resize", updateWidth);
+        return () => window.removeEventListener("resize", updateWidth);
+    }, []);
 
     return (
         <>
@@ -34,15 +43,15 @@ function Aspects() {
                 </div>
             </section>
 
-            <section className="map">
-                <div className="container-fluid map-container">
+            <section className="map" style={{ margin: "30px auto 50px" }}>
+                <div className="container map-container">
                     <div className="row align-items-center">
                         <div className="col-12 col-lg-4 coast-content">
                             <h2 style={{ fontWeight: "300" }}>
                                 Discover a World of <br /> Opportunities
                             </h2>
                             <p className="map-para">
-                                Invest globally in stocks, options, futures, currencies, bonds and funds from a single unified platform. Fund your account in multiple currencies and trade assets denominated in multiple currencies. Access market data 24 hours a day and six days a week.</p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
                             <div className="figures">
                                 <div className="row">
                                     <div className="col">
@@ -59,16 +68,43 @@ function Aspects() {
                                     </div>
                                 </div>
                             </div>
-                            <button className="btn btn-log btn-map">Learn More</button>
+                            <button className="btn btn-log btn-coast btn-map">Learn More</button>
                         </div>
                         <div className="col-12 col-lg-8">
                             <div className="map-img">
-                                <img src="https://i.postimg.cc/1twdcYLQ/Capture2.png" />
+                                <img style={{ width: "100%" }} src={width > 575 ? "https://i.postimg.cc/1twdcYLQ/Capture2.png" : "https://i.postimg.cc/sXNTcGQ6/Capture2.png"} />
                             </div>
                         </div>
                     </div>
                 </div>
 
+            </section>
+
+            <section className="coast-effective security">
+                <div className="container">
+                    <div className="row align-items-center">
+                        <div className="col-12 col-lg-5">
+                            <img className="cost-img" src="https://www.interactivebrokers.co.in/images/web/home-security.jpg" />
+                        </div>
+                        <div className="col-12 col-lg-6 coast-content">
+                            <h2 style={{ fontWeight: "300" }}>
+                                A Broker You Can Trust
+                            </h2>
+                            <p className="map-para">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+
+                            <ul className="coast-ul">
+                                <li>
+                                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                                </li>
+                                <li>
+                                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                                </li>
+                            </ul>
+                            <button className="btn btn-log btn-coast">IBKR Protection</button>
+                        </div>
+                    </div>
+                </div>
             </section>
         </>
     )
